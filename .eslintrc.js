c8cf29abf8501e30813dce0d4482473462f39597
Fernando de Paula <dipaula018@gmail.com>
2020-04-16 18:09:14 -0300

    feat: 18-monitoring errors queue

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,6 +18,7 @@ module.exports = {
   },
   rules: {
     "prettier/prettier": "error",
+    "no-console": "off",
     "class-methods-use-this": "off",
     "no-param-reassign":"off",
     "camelcase": "off",
