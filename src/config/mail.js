8a6b39ee29b5bb5425c32cdd47ef329fccfe72d7
Fernando de Paula <dipaula018@gmail.com>
2020-04-16 13:17:50 -0300

    feat: 15-nodemailer

---
+++ src/config/mail.js
@@ -0,0 +1,20 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '6319480e5c7c10',
+    pass: '6c80b5ff6bc8e3',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com',
+  },
+};
+
+// Amazon SES
+// Mailgun
+// Sparkpost
+// Mandril(Mailchip)
+// nao use GMAIl
+
+// Mailtrap(DEV)
